fbd3ac2d199aa1cbd56eaf8beaac97bddf8cc1fe
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2021-07-22 10:29:32 -0400

    Completed collatz-conjeture exercise.

---
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,16 @@
+
+
+export const steps = (number) => {
+  if (number <= 0) { throw new Error("Only positive numbers are allowed");}
+  let steps = 0;
+    while (number !== 1) {
+      if (number % 2 === 0) {
+        number = number / 2;
+      }
+      else {
+        number = (3*number) + 1;
+      }
+      steps++;
+    }
+  return steps;
+};

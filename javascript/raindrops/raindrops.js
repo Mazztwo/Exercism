deda8906d2479254583926a065030d673d6aa179
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2021-07-23 12:34:00 -0400

    Completed raindrops exercise.

---
+++ javascript/raindrops/raindrops.js
@@ -0,0 +1,8 @@
+export const convert = (number) => {
+  let rain = "";
+  if (number % 3 === 0) {rain+="Pling";}
+  if (number % 5 === 0) {rain+="Plang";}
+  if (number % 7 === 0) {rain+="Plong";}
+  if (rain.length === 0) {rain+=number;}
+  return rain;
+};

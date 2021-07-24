b74fc3500211ab98712f319b82b147e63f2edfdc
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2021-07-24 12:49:51 -0400

    Completed perfect-numbers exercise.

---
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -0,0 +1,16 @@
+export const classify = (number) => 
+{
+  if (number <= 0) {throw new Error('Classification is only possible for natural numbers.');}
+  let aliquotSum = 0;
+  for (let i = 1; i < number; i++)
+  {
+    if (number % i === 0)
+    {
+      aliquotSum += i;
+    }
+  }
+
+  if (aliquotSum === number) {return "perfect";}
+  else if (aliquotSum > number) {return "abundant";}
+  else { return "deficient";}
+};
